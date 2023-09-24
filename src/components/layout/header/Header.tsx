9fc65c69d9d4db63a94d9460389507fede54e6b4
Fredy228 <smagrovich58@gmail.com>
2023-09-24 19:53:29 +0300

    header-styled-for-pc

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,32 @@
 import { FC } from "react";
 
+import stylesCommon from "@/styles/common.module.scss";
+import styles from "./header.module.scss";
+import { IconNotific, IconSearch } from "@/components/reused/Icon/Icon";
+
 const Header: FC = () => {
-  return <header></header>;
+  return (
+    <header className={styles.header}>
+      <div className={stylesCommon.container}>
+        <div className={styles.header_inner}>
+          <div className={styles.header_userName}>
+            <p>Привіт Олекій,</p>
+          </div>
+          <ul className={styles.header_listNotificCenter}>
+            <li className={styles.header_itemNotificCenter}>
+              <IconSearch />
+            </li>
+            <li className={styles.header_itemNotificCenter}>
+              <IconNotific />
+              <div className={styles.header_wrapperCountNotificCenter}>
+                <span className={styles.header_countNotificCenter}>2</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/reused/Icon/Icon.tsx
+++ src/components/reused/Icon/Icon.tsx
@@ -1,1 +1,6 @@
+//Header icons
 export { default as IconSearch } from "./storage/search.svg";
+export { default as IconNotific } from "./storage/notific.svg";
+
+//SideBar icons
+export { default as IconArrowCouple } from "./storage/arrow-couple.svg";
