90efaca018d02ea507078c32ecebc2270bc222dc
Fredy228 <smagrovich58@gmail.com>
2023-10-20 19:47:23 +0300

    sidebar update

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -20,64 +20,74 @@ const Sidebar: FC = () => {
   const isClient = useSelector(selectCRS);
 
   return (
-    <aside className={styles.aside}>
-      <div className={styles.aside_wrapperLogo}>
-        <p className={styles.aside_logo}>
-          Intellecta<span>.</span>
-        </p>
-
-        <Image
-          className={styles.aside_logoV2}
-          src={`${process.env.NEXT_URL}/img/logo.webp`}
-          alt={"Logo"}
-          width={959}
-          height={959}
-        />
-      </div>
-      <div className={styles.aside_listMenuWrapper}>
-        <ul className={styles.aside_listMenu}>
-          {listMenu.map((item) => (
-            <li
-              key={item.id}
-              className={`${styles.aside_itemMenu} ${
-                item.href === pathname ? styles.active : ""
-              }`}
-            >
-              <Link href={item.href} className={styles.aside_linkMenu}>
-                <Image
-                  src={`${process.env.NEXT_URL}/img/sidebar/${item.url}`}
-                  alt={item.title}
-                  width={"30"}
-                  height={"30"}
-                  className={styles.aside_imgMenu}
-                />
-                <p className={styles.aside_linkText}>{item.title}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={styles.aside_wrapperUser}>
-        <Image
-          src={`${process.env.NEXT_URL}/img/sidebar/avatar.png`}
-          alt={"avatar"}
-          width={"40"}
-          height={"40"}
-          className={styles.aside_imgAvatar}
-          onClick={() =>
-            signOut({
-              callbackUrl: "/sign-in",
-            })
-          }
-        />
-        <div className={styles.aside_wrapperUserName}>
-          <p className={styles.aside_userName}>
-            {isClient ? `${user.firstName} ${user.lastName}` : <LoaderText />}
+    <aside
+      className={`${styles.aside} ${
+        pathname === "/dashboard" ? styles.active : ""
+      }`}
+    >
+      <div
+        className={`${styles.aside_inner} ${
+          pathname === "/dashboard" ? styles.active : ""
+        }`}
+      >
+        <div className={styles.aside_wrapperLogo}>
+          <p className={styles.aside_logo}>
+            Intellecta<span>.</span>
           </p>
-          <span className={styles.aside_userRole}>Студент 2-го курсу</span>
+
+          <Image
+            className={styles.aside_logoV2}
+            src={`${process.env.NEXT_URL}/img/logo.webp`}
+            alt={"Logo"}
+            width={959}
+            height={959}
+          />
         </div>
-        <div className={styles.aside_wrapperChangeIcon}>
-          <IconArrowCouple />
+        <div className={styles.aside_listMenuWrapper}>
+          <ul className={styles.aside_listMenu}>
+            {listMenu.map((item) => (
+              <li
+                key={item.id}
+                className={`${styles.aside_itemMenu} ${
+                  item.href === pathname ? styles.active : ""
+                }`}
+              >
+                <Link href={item.href} className={styles.aside_linkMenu}>
+                  <Image
+                    src={`${process.env.NEXT_URL}/img/sidebar/${item.url}`}
+                    alt={item.title}
+                    width={"30"}
+                    height={"30"}
+                    className={styles.aside_imgMenu}
+                  />
+                  <p className={styles.aside_linkText}>{item.title}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.aside_wrapperUser}>
+          <Image
+            src={`${process.env.NEXT_URL}/img/sidebar/avatar.png`}
+            alt={"avatar"}
+            width={"40"}
+            height={"40"}
+            className={styles.aside_imgAvatar}
+            onClick={() =>
+              signOut({
+                callbackUrl: "/sign-in",
+              })
+            }
+          />
+          <div className={styles.aside_wrapperUserName}>
+            <p className={styles.aside_userName}>
+              {isClient ? `${user.firstName} ${user.lastName}` : <LoaderText />}
+            </p>
+            <span className={styles.aside_userRole}>Студент 2-го курсу</span>
+          </div>
+          <div className={styles.aside_wrapperChangeIcon}>
+            <IconArrowCouple />
+          </div>
         </div>
       </div>
     </aside>
