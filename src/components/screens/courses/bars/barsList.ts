261327a7c14b5303c0ac3cf7280fb3f97f3cebda
ragn <gost45678@gmail.com>
2024-01-04 17:11:21 +0100

    finish

--- src/components/screens/courses/bars/barsList.ts
+++ src/components/screens/courses/bars/barsList.ts
@@ -1,8 +1,14 @@
 export type TBarsProps = {
+
     id:number;
     named:string;
-}
 
+}
+export interface ISupBarProps extends TBarsProps { 
+
+    images: string;
+    
+}
 export const barsLists: TBarsProps[] = [
     {
         id:1,
@@ -22,25 +28,30 @@ export const barsLists: TBarsProps[] = [
     },
 ]
 
-export const supBarsLists: TBarsProps[] = [
+export const supBarsLists: ISupBarProps[] = [
     {
         id:1,
-        named:'Front-end'
+        named:'Front-end',
+        images:`${process.env.NEXT_URL}/img/courses/html-icon.png`
     },
     {
         id:2,
-        named:'C#'
+        named:'C#',
+        images:`${process.env.NEXT_URL}/img/courses/sharp.png`
     },
     {
         id:3,
-        named:'Golang'
+        named:'Golang',
+        images:`${process.env.NEXT_URL}/img/courses/golang.png`
     },
     {
         id:4,
-        named:'JavaScript'
+        named:'JavaScript',
+        images:`${process.env.NEXT_URL}/img/courses/javaScript.png`
     },
     {
         id:5,
-        named:'Full-stack'
+        named:'Full-stack',
+        images:`${process.env.NEXT_URL}/img/courses/fullstack.png`
     },
 ]
