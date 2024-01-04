bb8627ad2d14e0b3dd86bf0850abfcc5df047fe8
ragn <gost45678@gmail.com>
2024-01-04 16:51:05 +0100

    created new screen

--- src/components/screens/courses/Courses.tsx
+++ src/components/screens/courses/Courses.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
-export default function CoursesPage() {
+import SupBar from './bars/SupBar';
+import TopBar from './bars/TopBar';
+import CardItem from './card/CardItem';
+import styles from './courses.module.scss'
+export default function Courses() {
   return (
-    <div>CoursesPage</div>
+    <div>        
+      <h2 className={styles.title}>нові курси вже доступні</h2>
+      <TopBar/>
+      <SupBar/>
+      <CardItem/>
+    </div>
   )
 }
+
+
+
