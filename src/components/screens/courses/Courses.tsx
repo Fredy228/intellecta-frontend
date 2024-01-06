b1a5b232cd44045031dcd5607b21a50d89b75f43
ragn <gost45678@gmail.com>
2024-01-06 19:58:58 +0100

    changes

--- src/components/screens/courses/Courses.tsx
+++ src/components/screens/courses/Courses.tsx
@@ -1,15 +1,35 @@
-import SupBar from './bars/SupBar';
-import TopBar from './bars/TopBar';
-import CardItem from './card/CardItem';
+import CoursesList from '@/components/ui/card/CoursesList/CoursesList'
+import CoursesListOther from '@/components/ui/card/CoursesList/CoursesListOther'
+import TestCardList from '@/components/ui/card/testCard/testCardList/TestCardList'
 import styles from './courses.module.scss'
+import Image from 'next/image'
 export default function Courses() {
   return (
-    <div>        
+    <main className={styles.list}>        
       <h2 className={styles.title}>нові курси вже доступні</h2>
-      <TopBar/>
-      <SupBar/>
-      <CardItem/>
-    </div>
+      <input placeholder='Пошук'/>
+      <div className={styles.cardWrapper}>
+        <CoursesList />
+        <div className={styles.cardContainer}>
+          <div>
+            <TestCardList />
+          </div>
+          <div className={styles.cardInner}>
+            <h1 className={styles.cardTitle}>Не знаєте, який курс обрати?</h1>
+            <Image src={`${process.env.NEXT_URL}/img/courses/cardtest.png`} alt='test img' width={150} height={151}/>
+            <p className={styles.cardDesc}>Ми створили тест, який вам допоможе обрати напрямок для вашого розвитку або професійного шляху.</p>
+          </div>
+        </div>
+        <div className={styles.cardWrapper}>
+          <CoursesListOther/>
+          <div className={styles.cardInner}>
+            <h1 className={styles.cardTitle}>Не знаєте, який курс обрати?</h1>
+            <Image src={`${process.env.NEXT_URL}/img/courses/cardtest.png`} alt='test img' width={150} height={151}/>
+            <p className={styles.cardDesc}>Ми створили тест, який вам допоможе обрати напрямок для вашого розвитку або професійного шляху.</p>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
