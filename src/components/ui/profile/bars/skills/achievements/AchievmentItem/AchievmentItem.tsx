464f425a05626ab067460189221d116bc09f736d
ragn <gost45678@gmail.com>
2024-01-20 11:54:23 +0100

    fix key prop

--- src/components/ui/profile/bars/skills/achievements/AchievmentItem/AchievmentItem.tsx
+++ src/components/ui/profile/bars/skills/achievements/AchievmentItem/AchievmentItem.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { FC, Fragment } from "react";
 import { TAchievivList } from "@/components/ui/profile/bars/skills/achievements/achieveList";
 import styles from "./AchievmentItem.module.scss";
-export const AchievmentItem: FC<TAchievivList> = ({ id, images, named }) => {
+export const AchievmentItem: FC<TAchievivList> = ({ images, named }) => {
   return (
     <>
-      <div key={id} className={styles.cardWrapper}>
+      <div className={styles.cardWrapper}>
         <Image src={images} alt={"card images"} width={75} height={75} />
         <li className={styles.named}>{named}</li>
       </div>
