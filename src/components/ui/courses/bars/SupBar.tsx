f7b2aa7ad06adef71fab47987c6d0ace537ee102
ragn <gost45678@gmail.com>
2024-01-22 19:08:15 +0100

    add hover

--- src/components/ui/courses/bars/SupBar.tsx
+++ src/components/ui/courses/bars/SupBar.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import { Fragment } from 'react'
 import { supBarsLists } from './barsList'
 import styles from './bar.module.scss'
+import { Fragment } from 'react'
+import Image from 'next/image'
 export default function SupBar() {
   return (
-    <div className={styles.supBarWrapper}>
+    <div className={styles.supBar_wrapper}>
         {supBarsLists.map(list => ( 
           <Fragment key={list.id}>
-            <button className={styles.btn}>
+            <button className={styles.supBar_btn}>
               <Image src={list.images} width={25} height={25} alt='courses icon'/>
               {list.named}
             </button>
