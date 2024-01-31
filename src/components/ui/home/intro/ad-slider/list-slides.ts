e9b78265a0570d46f4efc61dadfd0919aeab078f
Fredy228 <smagrovich58@gmail.com>
2024-01-31 15:30:04 +0200

    Fix slider

---
+++ src/components/ui/home/intro/ad-slider/list-slides.ts
@@ -0,0 +1,19 @@
+type SlideType = {
+  id: number;
+  image: string;
+};
+
+export const listSlides: SlideType[] = [
+  {
+    id: 1,
+    image: "img/ad/incubator-1.webp",
+  },
+  {
+    id: 2,
+    image: "img/ad/incubator-2.webp",
+  },
+  {
+    id: 3,
+    image: "img/ad/incubator-3.webp",
+  },
+];
