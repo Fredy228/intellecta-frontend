e5936826d0b8418c38843f86a7a60cbb3b4cc161
Fredy228 <smagrovich58@gmail.com>
2024-01-31 14:19:20 +0200

    Fix import

--- src/components/ui/courses/card/CourseItem/CourseItem.tsx
+++ src/components/ui/courses/card/CourseItem/CourseItem.tsx
@@ -2,10 +2,6 @@ import Link from "next/link";
 import { FC } from "react";
 import { TCoursesProps } from "../list";
 import styles from "./card.module.scss";
-import {
-  IconsImport,
-  listOptionsImg,
-} from "@/components/reused/Icon/IconsImport";
 
 export const CourseItem: FC<TCoursesProps> = ({
   types,
