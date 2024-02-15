4489752930805f0b7f8e6ba6608a72bd9cde448d
ragn <gost45678@gmail.com>
2024-02-15 22:22:45 +0100

    plug-test

---
+++ src/components/reused/partition-plug/PartitionPlugVariantOne.tsx
@@ -0,0 +1,38 @@
+import styles from "./plug.module.scss";
+export const PartitionPlugVariantOne = () => {
+    return (
+        <div className={styles.plug} >
+            <div className={styles.plug_wrapper}>
+                <h1 className={styles.plug_title}>Ви не підключені
+                    до жодного навчального процесу</h1>
+                <div className={styles.cube__wrapper}>
+                    <div className={styles.cube}>
+                        <div className={`${styles.front} ${styles.side}`}></div>
+                        <div className={`${styles.top} ${styles.side}`}></div>
+                        <div className={`${styles.right_side} ${styles.side}`}></div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.cube_wrapper_second}>
+                <div className={styles.cube_second}>
+                    <div className={`${styles.front_second} ${styles.side_second}`}></div>
+                    <div className={`${styles.right_side_second} ${styles.side_second}`}></div>
+                </div>
+            </div>
+            <div className={styles.star1}></div>
+            <div className={styles.star2}></div>
+            <div className={styles.star3}></div>
+            <div className={styles.star4}></div>
+            <div className={styles.star5}></div>
+            <div className={styles.star6}></div>
+            <div className={styles.star7}></div>
+            <div className={styles.star8}></div>
+            <div className={styles.star9}></div>
+            <div className={styles.star10}></div>
+            <div className={styles.star11}></div>
+            <div className={styles.star12}></div>
+            <div className={styles.star13}></div>
+        </div>
+    );
+};
+
--- src/components/screens/tests/Tests.tsx
+++ src/components/screens/tests/Tests.tsx
@@ -1,11 +1,12 @@
 import { type NextPage } from "next";
 import InDevelop from "@/components/ui/in-develop/InDevelop";
+import {PartitionPlugVariantOne} from "@/components/reused/partition-plug/PartitionPlugVariantOne";
 
 const Tests: NextPage = () => {
   return (
     <div>
       {" "}
-      <InDevelop name={"Тести"} />
+        <PartitionPlugVariantOne/>
     </div>
   );
 };
