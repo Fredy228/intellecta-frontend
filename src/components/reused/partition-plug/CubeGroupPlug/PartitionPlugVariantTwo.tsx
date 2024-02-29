a863ec81254c04fe764f0f9107cc0c2a1cfee0a0
Ragn <gost45678@gmail.com>
2024-02-29 21:22:17 +0100

    other

--- src/components/reused/partition-plug/CubeGroupPlug/PartitionPlugVariantTwo.tsx
+++ src/components/reused/partition-plug/CubeGroupPlug/PartitionPlugVariantTwo.tsx
@@ -1,11 +1,11 @@
-import styles from "./plug2.module.scss";
+import styles from "./plug.module.scss";
 export const PartitionPlugVariantTwo = () => {
     return (
-        <div className={styles.plug} >
+        <div className={`${styles.plug} ${styles.pink}`} >
             <div className={styles.plug_wrapper}>
                 <h1 className={styles.plug_title}>Ви не підключені
                     до жодного навчального процесу</h1>
-                <div className={styles.cube__wrapper}>
+                <div className={`${styles.cube__wrapper_right} `}>
                     <div className={styles.cube}>
                         <div className={`${styles.front} ${styles.side}`}></div>
                         <div className={`${styles.top} ${styles.side}`}></div>
@@ -13,11 +13,11 @@ export const PartitionPlugVariantTwo = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.cube_wrapper_second}>
+            <div className={`${styles.cube_wrapper_second_left}`}>
                 <div className={styles.cube_second}>
-                    <div className={`${styles.front_second} ${styles.side_second}`}></div>
-                    <div className={`${styles.top_second} ${styles.side_second}`}></div>
-                    <div className={`${styles.right_side_second} ${styles.side_second}`}></div>
+                    <div className={`${styles.front_second} ${styles.side}`}></div>
+                    <div className={`${styles.top_second} ${styles.side}`}></div>
+                    <div className={`${styles.right_side_second} ${styles.side}`}></div>
                 </div>
             </div>
             <div className={`${styles.star} ${styles.star1} ${styles.star_white}`}></div>
--- src/components/screens/chats/Chats.tsx
+++ src/components/screens/chats/Chats.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 
-import { PartitionPlugVariantFive } from "@/components/reused/partition-plug/PartitionPlugVariantFive";
+import { PartitionPlugVariantFive } from "@/components/reused/partition-plug/RobotGroupPlug/PartitionPlugVariantFive";
 
 const Chats: NextPage = () => {
   return <PartitionPlugVariantFive />;
